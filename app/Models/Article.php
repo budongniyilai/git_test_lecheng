df87f8aa2c0678b19db6516e069f1be26ae382aa
soultour <260483920@qq.com>
2018-01-22 19:04:26 +0800

    增加两个Model

---
+++ app/Models/Article.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * 功能：
+ * 操作: 唐强
+ * 日期: 1/22
+ * 时间: 7:00
+ */
+
+namespace App\Models;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class Article extends Model
+{
+    public $timestamps = false;
+}
---
+++ app/Models/Attachment.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * 功能：
+ * 操作: 唐强
+ * 日期: 1/22
+ * 时间: 7:01
+ */
+
+namespace App\Models;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class Attachment extends Model
+{
+    public $timestamps = false;
+}
